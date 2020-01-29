38afa98bfafa22dc0c5d48c57870dc981dd33fae
Vtsarehorodtseva <Vtsarehorodtseva@kpmg.ua>
2020-01-29 17:59:37 +0200

    plot2

---
+++ plot2.R
@@ -0,0 +1,28 @@
+library(readr)
+library(dplyr)
+library(lubridate)
+
+#create plots folder
+if (dir.exists("plots") == FALSE) {
+  dir.create("plots")
+}
+
+#data load
+data_path <- list.files(pattern = "household_power_consumption.txt",
+                        recursive = TRUE,
+                        ignore.case = TRUE)
+
+data <- read_delim(data_path, delim = ";")
+
+#date subset
+data_feb <- data %>% 
+  filter(dmy(Date) %within% interval(ymd("2007-02-01"), ymd("2007-02-02"))) %>%
+  #concatenating date and time
+  mutate(f_date = paste(Date, Time, sep = " "))
+
+#converting to POSIXct date format
+data_feb$f_date <- dmy_hms(data_feb$f_date)
+
+#linear ploting 
+plot(data_feb$f_date, data_feb$Global_active_power, 
+     type = "l", xlab = "", ylab = "Global Active Power")
