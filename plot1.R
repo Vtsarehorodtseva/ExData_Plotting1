dc29990c69efaadb1becbe99757e6f16c7b44f95
Vtsarehorodtseva <Vtsarehorodtseva@kpmg.ua>
2020-01-29 13:13:29 +0200

    plot1

---
+++ plot1.R
@@ -0,0 +1,29 @@
+library(readr)
+library(dplyr)
+library(lubridate)
+
+#create plots folder
+if (dir.exists("plots") == FALSE) {
+  dir.create("plots")
+}
+
+#data load
+data_path <- list.files(pattern = "household_power_consumption.txt",
+                        recursive = TRUE,
+                        ignore.case = TRUE)
+
+data <- read_delim(data_path, delim = ";")
+
+#date subset
+data_feb <- data %>% 
+  filter(dmy(Date) %within% interval(ymd("2007-02-01"), ymd("2007-02-02")))
+
+#histogram ploting
+hist(data_feb$Global_active_power, col = "red", 
+     main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)")
+
+#saving png file in plots folder
+dev.copy(png, file = "plots/plot1.png")
+dev.off()
+
