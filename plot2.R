0ef3f7e64873ea9609e78b52c1b37b3c1e0d525b
Vtsarehorodtseva <Vtsarehorodtseva@kpmg.ua>
2020-01-29 18:10:44 +0200

    plot2

--- plot2.R
+++ plot2.R
@@ -17,11 +17,8 @@ data <- read_delim(data_path, delim = ";")
 #date subset
 data_feb <- data %>% 
   filter(dmy(Date) %within% interval(ymd("2007-02-01"), ymd("2007-02-02"))) %>%
-  #concatenating date and time
-  mutate(f_date = paste(Date, Time, sep = " "))
-
-#converting to POSIXct date format
-data_feb$f_date <- dmy_hms(data_feb$f_date)
+  #concatenating date and time converting to POSIXct date format
+  mutate(f_date = dmy_hms(paste(Date, Time, sep = " ")))
 
 #linear ploting 
 plot(data_feb$f_date, data_feb$Global_active_power, 
